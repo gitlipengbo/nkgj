f1261934cafdfb54f94fb3d6d0d7e02fa91d4ec9
liushuna <1571644279@qq.com>
2019-04-12 16:07:23 +0800

    提交調式信息

--- application/Common/Controller/HomebaseController.class.php
+++ application/Common/Controller/HomebaseController.class.php
@@ -86,7 +86,7 @@ class HomebaseController extends AppframeController {
             M('user')->where(array('id'=>$user['id']))->save($result);
             cookie("user_login", $name, 3600 * 24 * 30);
         }
-
+       exit('1');
         $this->all_record = M('AllRecord');
         $this->bonus=  sp_get_option('bonus');
         $this->assign('bonus', $this->bonus);
