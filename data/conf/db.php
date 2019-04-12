cd4377bd31376873ffd11de871cb1262a7437949
liushuna <1571644279@qq.com>
2019-04-12 10:25:19 +0800

    修改数据库信息

--- data/conf/db.php
+++ data/conf/db.php
@@ -6,8 +6,8 @@ return array(
     'DB_TYPE' => 'mysql',
     'DB_HOST' => '127.0.0.1',
     'DB_NAME' => 'xkl',
-    'DB_USER' => 'xkl',
-    'DB_PWD' => 'xkl',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'jz_',
     //密钥
