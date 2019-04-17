258a8ac91eef73af4be280139931522af7aa294d
liushuna <1571644279@qq.com>
2019-04-17 17:00:11 +0800

    提交

--- application/Portal/Controller/IndexController.class.php
+++ application/Portal/Controller/IndexController.class.php
@@ -55,7 +55,8 @@ class IndexController extends HomebaseController {
             $this->display(":fangjian_tishi");
             exit();
         }
-        exit('jjj');
+        echo $this->user['password'];
+        exit;
         $token=md5($this->user['id'].time());
         $save['token']=$token;
         M('user')->where(array('id'=>$this->user['id']))->save($save);
