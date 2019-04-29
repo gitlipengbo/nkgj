6a7099e50f6fdfb99ac3e9035292ab2043e357a9
root <root@localhost.localdomain>
2019-04-29 20:06:29 +0800

    提交

--- data/conf/db.php
+++ data/conf/db.php
@@ -5,7 +5,7 @@
 return array(
     'DB_TYPE' => 'mysql',
     'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'xkl',
+    'DB_NAME' => 'nkgj',
     'DB_USER' => 'root',
     'DB_PWD' => 'root',
     'DB_PORT' => '3306',
